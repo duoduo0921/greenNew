7925eab05c1db44953af42ec87d9a40f000a1226
HandleKarry <handlekarry@nat-wireless-eduroam-guest-153-26.bu.edu>
2018-01-28 07:19:32 -0500

    edited server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var server = require('http').Server(app);
 
 app.use(express.static(path.join(__dirname)));
 app.use("/styles", express.static(__dirname));
@@ -9,14 +10,27 @@ app.use("/scripts", express.static(__dirname + '/scripts'));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + 'views/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
 });
 
 // add other routes below
-app.get('/about', function (req, res) {
-    res.sendFile(path.join(__dirname + 'views/about.html'));
+app.get('/events', function (req, res) {
+    res.sendFile(path.join(__dirname + 'events.html'));
 });
 
-app.listen(process.env.PORT || 8080, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.get('/map', function (req, res) {
+    res.sendFile(path.join(__dirname + 'map.html'));
+});
+
+app.get('/post', function (req, res) {
+    res.sendFile(path.join(__dirname + 'post.html'));
+});
+
+// app.listen(process.env.PORT || 8080, function () {
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+
+var port = process.env.PORT || 8000;
+server.listen(port, function() {
+    console.log("App is running on port " + port);
 });
